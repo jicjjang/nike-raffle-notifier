66752096d4e3c1f09fdd006aa2bbf13ee2a7f14a
jed.c <jed.c@linecorp.com>
2022-03-13 21:54:05 +0900

    :wrench: Modify cron time and alive message

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import "dotenv/config";
 import storyCheck from "./service/storyCheck";
 import feedCheck from "./service/feedCheck";
 import { Request, Response } from "express";
+import { sendMessage } from "./bot";
 
 export interface IStoryInformation {
   id: string;
@@ -65,12 +66,21 @@ const NIKE_STORE_INFOMATION: IStoryInformation[] = [
   },
 ];
 
+let count = 0;
+
 (async () => {
-  // 월화수목금 매시 15분마다 조회
-  cron.schedule("0 0/15 * * * 1,2,3,4,5", async () => {
-    await storyCheck(NIKE_STORE_INFOMATION);
-    await feedCheck(NIKE_STORE_INFOMATION);
-  });
+  // 월화수목금 정각으로부터 12분마다 조회
+  cron.schedule(
+    "0 0,12,24,36,48 10,11,12,13,14,15,16,17,18,19 * * *",
+    async () => {
+      await storyCheck(NIKE_STORE_INFOMATION);
+      await feedCheck(NIKE_STORE_INFOMATION);
+
+      if (count++ % 5 === 0) {
+        sendMessage("Still alive nike-raffle-notify");
+      }
+    }
+  );
 
   const PORT = process.env.PORT || 3000;
 
